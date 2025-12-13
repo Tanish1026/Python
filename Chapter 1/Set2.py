9377b7e3df6f52acee5c425fac64904b38c2a2bc
Tanish Kami <cse2023120@rcciit.org.in>
2025-12-13 15:31:02 +0530

    Add files via upload

---
+++ Chapter 1/Set2.py
@@ -0,0 +1,2 @@
+import pyttsx3
+pyttsx3.speak("Hey Tanish u r gonna do better")
---
+++ Chapter 1/Set3.py
@@ -0,0 +1,8 @@
+import os
+
+# Specify the directory you want to list
+directory = "/"
+
+# Print the contents of the directory
+contents = os.listdir(directory)
+print(contents)

e35daa9f94e8624ffe1edfd7d744c243d1144468
Tanish Kami <cse2023120@rcciit.org.in>
2025-12-16 21:05:10 +0530

    Add files via upload

---
+++ Chapter 4/01_list.py
@@ -0,0 +1,4 @@
+friends = ["Apple","Banana",3.15,True,"Cherry"]
+friends[0]= "Oranges"
+print(friends)
+print(friends[0:4])
---
+++ Chapter 4/02_list_methods.py
@@ -0,0 +1,13 @@
+friends = ["Apple","Banana",3.15,True,"Cherry"]
+friends.append("Oranges")
+friends.append("Tanish")
+print(friends)
+L1 = [23,44,9,15,52]
+# L1.sort()
+# L1.reverse()
+# L1.insert(3,35)
+# L1.pop(2)
+
+print(L1.remove(23))
+print(L1.pop(2))
+print(len(L1))
